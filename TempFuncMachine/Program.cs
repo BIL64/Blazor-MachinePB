e8f598eab2434a1cdfaa1a0723d96477a20ea563
Björn Lindqvist <bi-lqvist@live.se>
2023-02-27 14:24:13 +0100

    Function App.

---
+++ TempFuncMachine/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.Hosting;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    //app =>
+    //{
+    //    //app.UseMiddleware<>
+    //})
+    .ConfigureServices(services =>
+    {
+        // services.AddScoped<>();
+    })
+    .Build();
+
+host.Run();
